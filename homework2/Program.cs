6032331ccabc1d9b3402475dee93a12477e78437
Opanch88 <Pan159dom159@yandex.ru>
2023-07-27 22:03:10 +0300

    Прорешал все задачи , все ок

--- homework2/Program.cs
+++ homework2/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Task 10 .  Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
  
- /*
+/*
 
  int Number (int num)
  {
@@ -17,13 +17,14 @@ int threeDigit = Convert.ToInt32(Console.ReadLine());
 if (threeDigit < 100 || threeDigit >= 1000)
 {
    Console.WriteLine ("You have entered an incorrect three-digit number, please repeat the entry. "); 
-   return;
 }
+else 
+ {
  int res = Number(threeDigit);
- Console.WriteLine("Second digit of this number: " + res );
+ Console.Write("Second digit of this number: " + res );
+ }
 
 */
-
 
 // Task 13.   Задача 13: Напишите программу, которая выводит третью цифру заданного числа ( или -1, если третьей цифры нет.
 
@@ -44,14 +45,13 @@ if (digit <= 999)
    int res = Number (digit);
    Console.WriteLine("Three digit: " + res );
 }
-if   (digit <= 99)
+if (digit <= 99);
 {
    Console.WriteLine("Wrong number '-1' ");
 }
 
 
 */
-
 
 
 // Task 15.   Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
